6df9357e24108a228d223020c2e15c0f3c8c1798
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-04-24 21:31:14 +0300

    feat: мемоизировать EditAvatarPopup

--- src/components/AddPlacePopup.jsx
+++ src/components/AddPlacePopup.jsx
@@ -1,72 +1,74 @@
 import React, { useState, memo } from "react";
 
-const AddPlacePopup = memo(({ isOpen, onClose, onAddCard, isPending, changePending }) => {
-  const classNamePopup = `popup ${isOpen ? "popup_opened" : ""}`;
+const AddPlacePopup = memo(
+  ({ isOpen, onClose, onAddCard, isPending, changePending }) => {
+    const classNamePopup = `popup ${isOpen ? "popup_opened" : ""}`;
 
-  const [title, setTitle] = useState("");
-  const [link, setLink] = useState("");
+    const [title, setTitle] = useState("");
+    const [link, setLink] = useState("");
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
+    const handleTitleChange = (event) => {
+      setTitle(event.target.value);
+    };
 
-  const handleLinkChange = (event) => {
-    setLink(event.target.value);
-  };
+    const handleLinkChange = (event) => {
+      setLink(event.target.value);
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    changePending();
-    onAddCard({ name: title, link });
-  };
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      changePending();
+      onAddCard({ name: title, link });
+    };
 
-  return (
-    <div className={classNamePopup} id="popup-add">
-      <div className="popup__container">
-        <h2 className="popup__title">Новое место</h2>
-        <form
-          className="popup__form"
-          name="popupAddForm"
-          noValidate
-          onSubmit={handleSubmit}
-        >
-          <input
-            id="name-card-input"
-            type="text"
-            name="title"
-            className="popup__input popup__input_type_title"
-            placeholder="Название"
-            minLength="2"
-            maxLength="30"
-            required
-            onChange={handleTitleChange}
-          />
-          <span
-            name="name-card-input-error"
-            className="popup__input-error"
-          ></span>
-          <input
-            id="link-input"
-            type="url"
-            name="link"
-            className="popup__input popup__input_type_link"
-            placeholder="Ссылка на картинку"
-            required
-            onChange={handleLinkChange}
-          />
-          <span name="link-input-error" className="popup__input-error"></span>
-          <button className="popup__save-button" type="submit">
-            {isPending ? 'Создать...' : 'Создать'}
-          </button>
-        </form>
-        <button
-          className="popup__close"
-          type="button"
-          onClick={onClose}
-        ></button>
+    return (
+      <div className={classNamePopup} id="popup-add">
+        <div className="popup__container">
+          <h2 className="popup__title">Новое место</h2>
+          <form
+            className="popup__form"
+            name="popupAddForm"
+            noValidate
+            onSubmit={handleSubmit}
+          >
+            <input
+              id="name-card-input"
+              type="text"
+              name="title"
+              className="popup__input popup__input_type_title"
+              placeholder="Название"
+              minLength="2"
+              maxLength="30"
+              required
+              onChange={handleTitleChange}
+            />
+            <span
+              name="name-card-input-error"
+              className="popup__input-error"
+            ></span>
+            <input
+              id="link-input"
+              type="url"
+              name="link"
+              className="popup__input popup__input_type_link"
+              placeholder="Ссылка на картинку"
+              required
+              onChange={handleLinkChange}
+            />
+            <span name="link-input-error" className="popup__input-error"></span>
+            <button className="popup__save-button" type="submit">
+              {isPending ? "Создать..." : "Создать"}
+            </button>
+          </form>
+          <button
+            className="popup__close"
+            type="button"
+            onClick={onClose}
+          ></button>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default AddPlacePopup;
--- src/components/EditAvatarPopup.jsx
+++ src/components/EditAvatarPopup.jsx
@@ -1,53 +1,55 @@
-import React, { useRef } from "react";
+import React, { useRef, memo } from "react";
 
-const EditAvatarPopup = ({ isOpen, onClose, onUpdateAvatar, isPending, changePending }) => {
-  const inputRef = useRef(null);
+const EditAvatarPopup = memo(
+  ({ isOpen, onClose, onUpdateAvatar, isPending, changePending }) => {
+    const inputRef = useRef(null);
 
-  const classNamePopup = `popup ${isOpen ? "popup_opened" : ""}`;
+    const classNamePopup = `popup ${isOpen ? "popup_opened" : ""}`;
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    changePending();
-    onUpdateAvatar({
-      avatar: inputRef.current.value,
-    });
-  };
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      changePending();
+      onUpdateAvatar({
+        avatar: inputRef.current.value,
+      });
+    };
 
-  return (
-    <div className={classNamePopup} id="popup-update-avatar">
-      <div className="popup__container">
-        <h2 className="popup__title">Обновить аватар</h2>
-        <form
-          className="popup__form"
-          name="popupUpdateAvatarForm"
-          noValidate
-          onSubmit={handleSubmit}
-        >
-          <input
-            id="link-avatar-input"
-            type="url"
-            name="avatar"
-            className="popup__input popup__input_type_link"
-            placeholder="Ссылка на картинку профиля"
-            required
-            ref={inputRef}
-          />
-          <span
-            id="link-avatar-input-error"
-            className="popup__input-error"
-          ></span>
-          <button className="popup__save-button" type="submit">
-          {isPending ? 'Сохранить...' : 'Сохранить'}
-          </button>
-        </form>
-        <button
-          className="popup__close"
-          type="button"
-          onClick={onClose}
-        ></button>
+    return (
+      <div className={classNamePopup} id="popup-update-avatar">
+        <div className="popup__container">
+          <h2 className="popup__title">Обновить аватар</h2>
+          <form
+            className="popup__form"
+            name="popupUpdateAvatarForm"
+            noValidate
+            onSubmit={handleSubmit}
+          >
+            <input
+              id="link-avatar-input"
+              type="url"
+              name="avatar"
+              className="popup__input popup__input_type_link"
+              placeholder="Ссылка на картинку профиля"
+              required
+              ref={inputRef}
+            />
+            <span
+              id="link-avatar-input-error"
+              className="popup__input-error"
+            ></span>
+            <button className="popup__save-button" type="submit">
+              {isPending ? "Сохранить..." : "Сохранить"}
+            </button>
+          </form>
+          <button
+            className="popup__close"
+            type="button"
+            onClick={onClose}
+          ></button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default EditAvatarPopup;
