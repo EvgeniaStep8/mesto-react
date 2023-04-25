6065d24407de2a6e8b96467102f06d3f5fd9f1c1
Evgenia Stepanova <evgeniast85es@gmail.com>
2023-04-25 12:08:28 +0300

    feat: мемоизировать компонент main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,65 +1,67 @@
-import React, { useContext } from "react";
+import React, { memo, useContext } from "react";
 import Card from "./Card";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import { CurrentCardsContext } from "../contexts/CurrentCardsContext";
 
-const Main = ({
-  onEditAvatar,
-  onEditProfile,
-  onAddCard,
-  onCardClick,
-  onCardLikeClick,
-  onCardDelete,
-}) => {
-  const currentUser = useContext(CurrentUserContext);
-  const currentCards = useContext(CurrentCardsContext);
+const Main = memo(
+  ({
+    onEditAvatar,
+    onEditProfile,
+    onAddCard,
+    onCardClick,
+    onCardLikeClick,
+    onCardDelete,
+  }) => {
+    const currentUser = useContext(CurrentUserContext);
+    const currentCards = useContext(CurrentCardsContext);
 
-  return (
-    <main className="content">
-      <section className="profile">
-        <div className="profile__container">
-          <button
-            className="profile__change-avatar-button"
-            type="button"
-            onClick={onEditAvatar}
-          >
-            <img
-              className="profile__avatar"
-              src={currentUser.avatar}
-              alt="Фотография профиля"
-            />
-          </button>
-          <div className="profile__info">
-            <h1 className="profile__name">{currentUser.name}</h1>
+    return (
+      <main className="content">
+        <section className="profile">
+          <div className="profile__container">
             <button
-              className="profile__edit-button"
+              className="profile__change-avatar-button"
               type="button"
-              onClick={onEditProfile}
-            ></button>
-            <p className="profile__job">{currentUser.about}</p>
+              onClick={onEditAvatar}
+            >
+              <img
+                className="profile__avatar"
+                src={currentUser.avatar}
+                alt="Фотография профиля"
+              />
+            </button>
+            <div className="profile__info">
+              <h1 className="profile__name">{currentUser.name}</h1>
+              <button
+                className="profile__edit-button"
+                type="button"
+                onClick={onEditProfile}
+              ></button>
+              <p className="profile__job">{currentUser.about}</p>
+            </div>
           </div>
-        </div>
-        <button
-          className="profile__add-button"
-          type="button"
-          onClick={onAddCard}
-        ></button>
-      </section>
-      <section className="cards">
-        {currentCards?.map((card) => (
-          <Card
-            key={card._id}
-            card={card}
-            isOwnerCard={card.owner._id === currentUser._id}
-            isLiked={card.likes.some((like) => like._id === currentUser._id)}
-            onCardClick={onCardClick}
-            onCardLikeClick={onCardLikeClick}
-            onCardDelete={onCardDelete}
-          />
-        ))}
-      </section>
-    </main>
-  );
-};
+          <button
+            className="profile__add-button"
+            type="button"
+            onClick={onAddCard}
+          ></button>
+        </section>
+        <section className="cards">
+          {currentCards?.map((card) => (
+            <Card
+              key={card._id}
+              card={card}
+              isOwnerCard={card.owner._id === currentUser._id}
+              isLiked={card.likes.some((like) => like._id === currentUser._id)}
+              onCardClick={onCardClick}
+              onCardLikeClick={onCardLikeClick}
+              onCardDelete={onCardDelete}
+            />
+          ))}
+        </section>
+      </main>
+    );
+  }
+);
 
 export default Main;
