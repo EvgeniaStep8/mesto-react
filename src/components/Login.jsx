2c02b3f7c21bf5cd2d7892e0750f39b31184de2a
Evgenia Stepanova <evgeniast85es@gmail.com>
2023-05-02 16:05:34 +0300

    style: создать отдельные стили для формы для переиспользования

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,27 @@ import React from "react";
 
 const Login = () => {
   return (
-		<h1>Login</h1>
+		<div className="authorization">
+			<h1>Регистрация</h1>
+			<form className="authorization__form">
+				<input
+				  type="email"
+				  className="popup__input"
+					placeholder="Email"
+				/>
+				<input
+				  type="password"
+					className="popup__input"
+					placeholder="Пароль"
+				/>
+				<button className="authorization__submit-button">
+				  Зарегистрироваться
+				</button>
+			</form>
+			<p className="authorization__info">
+			  Уже зарегистрированы? Войти
+			</p>
+		</div>
 	)
 }
 
