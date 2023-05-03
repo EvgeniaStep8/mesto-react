1cf6c6d46cd5da171d8fd105381e7e6b387bf003
Evgenia Stepanova <evgeniast85es@gmail.com>
2023-05-03 12:07:39 +0300

    feat: добавит стили для фоормы в тёмном стиле

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,14 +7,20 @@ const Login = () => {
 			<form className="form">
 				<input
 				  type="email"
-				  className="form__input"
+				  className="form__input form__input_theme_dark"
+					name="login"
 					placeholder="Email"
+					required
 				/>
+				<span id="login-input-error" className="form__input-error"></span>
 				<input
 				  type="password"
-					className="form__input"
+					className="form__input form__input_theme_dark"
+					name="password"
 					placeholder="Пароль"
+					required
 				/>
+        <span id="password-input-error" className="form__input-error"></span>
 				<button className="form__submit-button form__submit-button_type_authorization">
 				  Зарегистрироваться
 				</button>
